6c0079305505e228d4c126a45b4c4583707fd63e
Kieran <bmxstreet@gmx.net>
2017-04-18 09:54:41 +0200

    Update image-proxy.php

--- image-proxy.php
+++ image-proxy.php
@@ -1,12 +1,20 @@
 <?php
 //set_time_limit(10); //Thought this overrides the settings but apparently only appends, so rip me.
 
+function getUrlMimeType($url) {
+    $buffer = file_get_contents($url);
+    $finfo = new finfo(FILEINFO_MIME_TYPE);
+    return $finfo->buffer($buffer);
+}
+
 $image = htmlentities($_GET['url']);
 $ext = substr($image, -3); //Couldn't think of a better idea...
 
+$ext = getUrlMimeType($image);
+
 switch ($ext) {
 	case 'jpg':
-		$mime = 'image/jpeg';
+	$mime = 'image/jpeg';
         break;
     case 'gif':
         $mime = 'image/gif';
